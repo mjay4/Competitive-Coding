f30043c4329da053b71f48053488b649eb0c77d0
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-08-20 11:00:07 +0530

    Some_Important_Functions

---
+++ Others/IsPrime.java
@@ -0,0 +1,14 @@
+static boolean[] SeiveOfEratoSthenes(int n){
+
+    boolean isPrime[] = new boolean[n+1];
+
+    Arrays.fill(isPrime, true);
+    isPrime[0] = false, isPrime[1] = false;
+
+    for(int i =0;i*i<=n;i++){
+        for(int j = i+i; j<=n; j+=i){
+            isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
