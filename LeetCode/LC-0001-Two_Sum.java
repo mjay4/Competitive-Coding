ac4b7de56c932853e8c68dbb336f97070d03c1d7
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-08-30 15:38:50 +0530

    LC-0001 ADDED

---
+++ LeetCode/LC-0001-Two_Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int res[] = new int[2];
+        
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i = 0; i < nums.length;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                res[0] = map.get(nums[i]);
+                res[1] = i; break;
+            }
+            else
+            {
+                map.put((target - nums[i]),i);
+            }
+        }
+        
+        return res;
+    }
+}
