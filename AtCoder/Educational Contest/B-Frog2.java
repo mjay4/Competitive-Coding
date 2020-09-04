e7b9c1771242c360fec454d6f17437f120226c86
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-09-04 16:39:47 +0530

    Atcoder updated

---
+++ AtCoder/Educational Contest/B-Frog2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+     	int n = Integer.parseInt(st.nextToken());
+     	int k = Integer.parseInt(st.nextToken());
+     	int ar[] = new int[n];
+   		int dp[] = new int[n];
+   		st = new StringTokenizer(br.readLine());
+       	for(int i =0;i<n;i++) {
+       		ar[i] = Integer.parseInt(st.nextToken());
+       	}
+       
+       	dp[0] = 0;
+       	for(int i =1;i<n; i++) {
+       		dp[i] = Integer.MAX_VALUE;
+       		for(int j = i-1; j>=Math.max(0, i-k); j--)
+       			dp[i] = Math.min(dp[i] , Math.abs(ar[i] - ar[j]) + dp[j]);
+       	}
+       	
+       	System.out.println(dp[n-1]);
+     
+       
+	}
+}
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Solutions to some of the coding problems from popular websites.
 
 ## Stats
 
-Number of problems solved: **93**
+Number of problems solved: **95**
 
 ### LeetCode
 
@@ -38,4 +38,4 @@ Number of problems solved: **93**
 * Important Function: 5
 * GFG: 1
 * Google: 0
-* Atcoder : 1
+* Atcoder : 2
