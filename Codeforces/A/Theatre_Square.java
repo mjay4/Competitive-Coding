8289fd594d67b5ae45538bb5de3ab908ec029471
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-09-09 19:50:18 +0530

    Commited

---
+++ Codeforces/A/Theatre_Square.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Theatre_Square {
+
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        System.out.println((long) (Math.ceil(n * 1.0 / a) * Math.ceil(m * 1.0 / a)));
+    }
+
+    static final Random random = new Random();
+
+    static void sort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int val = random.nextInt(n);
+            int cur = a[i];
+            a[i] = a[val];
+            a[val] = cur;
+        }
+        Arrays.sort(a);
+    }
+
+    static void sortl(long[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int val = random.nextInt(n);
+            long cur = a[i];
+            a[i] = a[val];
+            a[val] = cur;
+        }
+        Arrays.sort(a);
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Solutions to some of the coding problems from popular websites.
 
 ## Stats
 
-Number of problems solved: **112**
+Number of problems solved: **113**
 
 ### LeetCode
 
@@ -34,7 +34,7 @@ Number of problems solved: **112**
 ### Others
 
 * Facebook: 4
-* Codeforces: 23
+* Codeforces: 24
 * Important Function: 5
 * GFG: 1
 * Google: 0
