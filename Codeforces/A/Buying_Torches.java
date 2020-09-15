39b0743c43c2ed55348cc0ff9372b6a9f2436114
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-09-15 18:53:20 +0530

    Educational round

---
+++ Codeforces/A/Buying_Torches.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Buying_Torches {
+	
+	public static void main(String[] args) {
+		FastScanner sc=new FastScanner();
+		int T=sc.nextInt();
+		for (int tt=0; tt<T; tt++) {
+            long x = sc.nextInt();
+            long y = sc.nextInt();
+            long k = sc.nextInt();
+            long coal = k*y;
+            long ans = (k+coal-1)/(x-1); 
+            if((k+coal-1)%(x-1) > 0)
+                ans++;
+                
+            System.out.println(ans+k);
+        }
+	}
+	static final Random random = new Random();
+	
+	static void sort(int[] a) {
+		int n = a.length;
+		for(int i =0; i<n; i++) {
+			int val = random.nextInt(n);
+			int cur = a[i];
+			a[i] = a[val];
+			a[val] = cur;
+		}
+		Arrays.sort(a);
+	}
+	
+	static void sortl(long[] a) {
+		int n = a.length;
+		for(int i =0; i<n; i++) {
+			int val = random.nextInt(n);
+			long cur = a[i];
+			a[i] = a[val];
+			a[val] = cur;
+		}
+		Arrays.sort(a);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (st == null  || !st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		String nextLine() { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+		} 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+	}
+
+}
