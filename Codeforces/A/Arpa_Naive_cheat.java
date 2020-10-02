4ff9b377c7b5186e7d57b82254e6e6f7c52690fc
mjay4 <45495121+mjay4@users.noreply.github.com>
2020-10-02 10:43:53 +0530

    codeforces 383 added

---
+++ Codeforces/A/Arpa_Naive_cheat.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Arpa_Naive_cheat {
+
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int n = sc.nextInt();
+        int ans = 0;
+        switch(n%4){
+            case 0 : ans = 6;
+                    break;
+            case 1 : ans = 8;
+                    break;
+            case 2 : ans = 4;
+                    break;
+            case 3 : ans = 2;  
+        }
+        if(n==0)
+            ans = 1;
+        System.out.println(ans);
+    }
+
+    public static int fact(int n) {
+        int ans = 1;
+        for (int i = 2; i <= n; i++)
+            ans *= i;
+        return ans;
+    }
+
+    static final Random random = new Random();
+
+    static void sort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int val = random.nextInt(n);
+            int cur = a[i];
+            a[i] = a[val];
+            a[val] = cur;
+        }
+        Arrays.sort(a);
+    }
+
+    static void sortl(long[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int val = random.nextInt(n);
+            long cur = a[i];
+            a[i] = a[val];
+            a[val] = cur;
+        }
+        Arrays.sort(a);
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Solutions to some of the coding problems from popular websites.
 
 ## Stats
 
-Number of problems solved: **148**
+Number of problems solved: **150**
 
 ### LeetCode
 
@@ -34,7 +34,7 @@ Number of problems solved: **148**
 ### Others
 
 * Facebook: 4
-* Codeforces: 44
+* Codeforces: 46
 * Important Function: 6
 * GFG: 1
 * Google: 4
