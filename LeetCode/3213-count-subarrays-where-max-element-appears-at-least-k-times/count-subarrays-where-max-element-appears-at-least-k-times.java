ce65b87cea18fbc673e0071a1ba0496209be4392
Mritunjay Choubey <choubeymritunjay3@gmail.com>
2024-03-29 19:05:43 +0530

    Time: 6 ms (39.29%) | Memory: 61.7 MB (66.84%) - LeetSync

--- LeetCode/3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ LeetCode/3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,21 +1,25 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        long ans =0;
-        long max =-1;
-        List<Integer> maxElement = new ArrayList<>();
-        for (int i=0; i<nums.length; i++) {
-            if(nums[i]>max) {
-                max = nums[i];
-                maxElement = new ArrayList<>();
-                ans = 0;
-            } 
-            if (nums[i] == max) {
-                maxElement.add(i+1);
+        long maxElement = -1;
+        long len = nums.length;
+        long ans =0; 
+        long left =0, right =0;
+        long maxElementCount =0;
+
+        for (int i =0; i<len; i++) maxElement = Math.max(maxElement, nums[i]);
+
+        while (left <= right && right < len) {
+
+            if (nums[(int)right] == maxElement) maxElementCount++;
+
+            while (maxElementCount >= k) {
+                if (nums[(int)left] == maxElement) maxElementCount--;
+                left++;
+
+                ans+= len-right;
             }
 
-            if(maxElement.size() >= k) {
-                ans+=maxElement.get(maxElement.size()-k);
-            }
+            right++;
         }
 
         return ans;
