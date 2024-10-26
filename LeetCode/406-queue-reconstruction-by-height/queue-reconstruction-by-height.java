99ca28f3d080c5912716a8dc4e26c7a50b578303
Mritunjay Choubey <choubeymritunjay3@gmail.com>
2024-10-26 20:49:14 +0530

    Time: 6 ms (97.30%) | Memory: 45.5 MB (28.51%) - LeetSync

---
+++ LeetCode/406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,18 @@
+class Solution {
+       public int[][] reconstructQueue(int[][] people) {
+        //pick up the tallest guy first
+        //when insert the next tall guy, just need to insert him into kth position
+        //repeat until all people are inserted into list
+        Arrays.sort(people,new Comparator<int[]>(){
+           @Override
+           public int compare(int[] o1, int[] o2){
+               return o1[0]!=o2[0]?-o1[0]+o2[0]:o1[1]-o2[1];
+           }
+        });
+        List<int[]> res = new ArrayList<>();
+        for(int[] cur : people){
+            res.add(cur[1],cur);       
+        }
+        return res.toArray(new int[people.length][]);
+    }
+}
