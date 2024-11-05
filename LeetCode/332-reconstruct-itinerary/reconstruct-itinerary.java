9f7f948630cba94bfb1f0c80cbc028de1d9cc1cd
Mritunjay Choubey <choubeymritunjay3@gmail.com>
2024-11-05 21:26:21 +0530

    Time: 6 ms (74.19%) | Memory: 45.6 MB (8.09%) - LeetSync

---
+++ LeetCode/332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    List<String> res = new ArrayList();
+    Map<String, PriorityQueue<String>> map = new HashMap<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+
+        for(List<String> ticket : tickets) {
+            map.computeIfAbsent(ticket.get(0), k -> new PriorityQueue()).add(ticket.get(1));
+        }
+        
+        // We always start from JFK. 
+        dfs("JFK");
+        return res;
+    }
+    
+    private void dfs(String curr) {
+        while (map.containsKey(curr) && ! map.get(curr).isEmpty()) {
+            dfs(map.get(curr).poll());
+        }
+        
+        res.add(0, curr);
+    }
+}
