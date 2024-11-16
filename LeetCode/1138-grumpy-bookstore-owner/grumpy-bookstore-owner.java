368a04ec00adb2cca9b58dc36d20ec57e2ec6fff
Mritunjay Choubey <choubeymritunjay3@gmail.com>
2024-11-16 17:36:47 +0530

    Time: 4 ms (45.76%) | Memory: 47.4 MB (62.56%) - LeetSync

--- LeetCode/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ LeetCode/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,28 +1,29 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int n = customers.length;
-        int maxSum = 0;
         int runningSum = 0;
         int ans = 0;
 
         for (int i=0; i<minutes; i++) {
             if (grumpy[i] == 0) {
+                // If Not Grumpy, then customer is satisfied by default. 
                 ans+=customers[i];
                 customers[i] = 0;
             } else {
+                // Running sum for the first m minutes. 
                 runningSum += customers[i];
             }
         }
 
-        maxSum = runningSum;
+        int maxSum = runningSum;
 
         for (int i=minutes; i<n; i++) {
             int idx = i-minutes;
- 
             if (grumpy[i] == 0) {
                 ans+=customers[i];
                 customers[i] = 0; 
             } 
+            // Running sum for the window. (When grumpy = 1)
             runningSum += customers[i]-customers[idx];
             maxSum = Math.max(maxSum,runningSum);
         }
